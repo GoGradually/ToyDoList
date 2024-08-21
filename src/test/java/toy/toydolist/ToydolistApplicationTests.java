e882e24c949e707edb7e3a75942edba9755c8159
GoGradually <chkun3109@gmail.com>
2024-08-21 22:04:14 +0900

    프로젝트 생성

---
+++ src/test/java/toy/toydolist/ToydolistApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.toydolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToydolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
