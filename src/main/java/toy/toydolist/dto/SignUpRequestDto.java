c8aa887124cea7b18cafa573f4b10634de74e399
GoGradually <chkun3109@gmail.com>
2024-08-23 03:52:57 +0900

    회원가입, 로그인 DTO validation 적용

--- src/main/java/toy/toydolist/dto/SignUpRequestDto.java
+++ src/main/java/toy/toydolist/dto/SignUpRequestDto.java
@@ -1,5 +1,6 @@
 package toy.toydolist.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -7,10 +8,12 @@ import org.hibernate.validator.constraints.Length;
 @Data
 public class SignUpRequestDto {
     @NotNull
+    @NotBlank
     @Length(min=6, max=20)
     String username;
 
     @NotNull
+    @NotBlank
     @Length(min=6, max=20)
     String password;
 }
